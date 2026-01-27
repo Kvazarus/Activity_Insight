cbfaa995cc62461b0575d08c8f50cda685836c2d
kvazarus <kvazarus13@yandex.ru>
2026-01-27 21:24:29 +0300

    Небольшая полировка

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 // TODO: прикрутить базу данных, сделать обработчик сигналов (Ctrl+C и тд), добавить треды
 // TODO: добавить пользователям возможность сменить имя программы
+// TODO: сделать обработку вкладок браузера (в том числе обновить цикл в main)
+// TODO: добавить GetAncestor для повышения вероятности успешного считывания окна (а может и не надо, пока всё работает и без него)
 
 #include <windows.h>
 #include <iostream>
@@ -9,12 +11,12 @@
 #include <fcntl.h>
 #include <vector>
 #include <strsafe.h>
+#include <filesystem>
 
 struct WindowData {
     HWND window_handle = nullptr;
     DWORD process_id = 0;
     std::wstring window_title;
-    HANDLE process_handle = nullptr;
     std::wstring exe_filename;
     std::string error;
 
@@ -26,15 +28,15 @@ struct WindowData {
 WindowData getWindowData() {
     const int string_max_length = 1024;
     WindowData window_data;
-    wchar_t buffer[string_max_length] = {0};
+    wchar_t window_text_buffer[string_max_length] = {0};
 
     window_data.window_handle = GetForegroundWindow();
     if (!window_data.window_handle) {
         window_data.error = "GetForegroundWindow error";
         return window_data;
     }
-    if (GetWindowTextW(window_data.window_handle, buffer, string_max_length) > 0) {
-        window_data.window_title = buffer;
+    if (GetWindowTextW(window_data.window_handle, window_text_buffer, string_max_length) > 0) {
+        window_data.window_title = window_text_buffer;
     }
     LPDWORD process_id_ptr = &window_data.process_id;
     GetWindowThreadProcessId(window_data.window_handle, process_id_ptr);
@@ -42,16 +44,17 @@ WindowData getWindowData() {
         window_data.error = "GetWindowThreadProcessId error";
         return window_data;
     }
-    window_data.process_handle = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION,
-                                             false, window_data.process_id);
-    if (window_data.process_handle) {
+    HANDLE process_handle = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION,
+                                        false, window_data.process_id);
+    if (process_handle) {
+        wchar_t image_name_buffer[string_max_length] = {0};
         DWORD image_name_size = string_max_length;
-        if (!QueryFullProcessImageNameW(window_data.process_handle, 0,
-                                        buffer, &image_name_size)) {
+        if (!QueryFullProcessImageNameW(process_handle, 0,
+                                        image_name_buffer, &image_name_size)) {
             window_data.error = "QueryFullProcessImageNameW error";
         }
-        window_data.exe_filename = buffer;
-        CloseHandle(window_data.process_handle);
+        window_data.exe_filename = image_name_buffer;
+        CloseHandle(process_handle);
     } else {
         window_data.error = "OpenProcess error";
     }
@@ -59,11 +62,7 @@ WindowData getWindowData() {
 }
 
 std::wstring getAppNameFromPath(const std::wstring &exe_filename) {
-    int pos = 0;
-    for (int i = 0; i < exe_filename.size(); i++) {
-        if (exe_filename[i] == '\\') pos = i + 1;
-    }
-    return exe_filename.substr(pos, exe_filename.size() - pos - 4);
+    return std::filesystem::path(exe_filename).stem().wstring();
 }
 
 std::wstring getAppName(const std::wstring &exe_filename) {
@@ -71,7 +70,8 @@ std::wstring getAppName(const std::wstring &exe_filename) {
     uint32_t buffer_size = GetFileVersionInfoSizeW(exe_filename.c_str(), &dummy);
     if (!buffer_size) return getAppNameFromPath(exe_filename);
     std::vector<BYTE> buffer(buffer_size);
-    if (!GetFileVersionInfoW(exe_filename.c_str(), 0, buffer_size, buffer.data())) return getAppNameFromPath(exe_filename);
+    if (!GetFileVersionInfoW(exe_filename.c_str(), 0, buffer_size, buffer.data()))
+        return getAppNameFromPath(exe_filename);
 
     HRESULT hr;
     struct LANGANDCODEPAGE {
@@ -87,13 +87,14 @@ std::wstring getAppName(const std::wstring &exe_filename) {
     for (int i = 0; i < (data_size / sizeof(LANGANDCODEPAGE)); i++) {
         wchar_t lang_and_codepage_str[100];
         hr = StringCchPrintfW(lang_and_codepage_str, 100, L"\\StringFileInfo\\%04x%04x\\FileDescription",
-                             lpTranslate[i].wLanguage, lpTranslate[i].wCodePage);
+                              lpTranslate[i].wLanguage, lpTranslate[i].wCodePage);
         if (FAILED(hr)) continue;
 
         wchar_t *app_name = nullptr;
         uint32_t app_data_size = 0;
 
-        if (VerQueryValueW(buffer.data(), lang_and_codepage_str, (LPVOID*) &app_name, &app_data_size) && app_data_size > 0) {
+        if (VerQueryValueW(buffer.data(), lang_and_codepage_str, (LPVOID *) &app_name, &app_data_size)
+            && app_data_size > 0) {
             return std::wstring(app_name);
         }
     }
@@ -105,7 +106,6 @@ void printWindowData(WindowData &window_data) {
     std::wcout << L"Handle: " << window_data.window_handle << std::endl;
     std::wcout << L"Process Id: " << window_data.process_id << std::endl;
     std::wcout << L"Window Title: " << window_data.window_title << std::endl;
-    std::wcout << L"Window Process Handle: " << window_data.process_handle << std::endl;
     std::wcout << L"Window Exe Filename: " << window_data.exe_filename << std::endl;
 
     // TODO: закешировать app name
